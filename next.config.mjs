a8e837f8ce37da0e325cd827ce8ee8e0f47b11c0
meshach567 <arinzemeshach123@gmail.com>
2024-07-03 12:51:59 +0100

    commit to main

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-
-
 const nextConfig = {
-    // Your Next.js configuration here
-    images: { unoptimized: true },
-    
-  };
-  
-  export default nextConfig;
-  
+  // Your Next.js configuration here
+  images: { unoptimized: true },
+   basePath: "/traveltour",
+  output: "export", // <=== enables static exports
+  reactStrictMode: true,
+};
+
+export default nextConfig;
